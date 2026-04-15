987a79ce52cda55e7faebbf54f8d0361928755fc
Alec Pullen <alecpullen24@gmail.com>
2026-04-15 11:40:42 +1000

    edu login button

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -39,11 +39,11 @@ function Login() {
         }
     }
 
-    // TODO: Social login with BetterAuth
-    const handleSocialLogin = (provider) => {
-        console.log(`Social login with ${provider} - integrate with BetterAuth`)
-        // import { signInSocial }
-        // await signInSocial({ provider, callbackURL: '/bookings' })
+    // TODO: AAF (Australian Access Federation) SSO integration
+    const handleAAFLogin = () => {
+        console.log('AAF SSO - integrate with BetterAuth')
+        // import { signInSSO }
+        // await signInSSO({ provider: 'aaf', callbackURL: '/bookings' })
     }
 
     return (
@@ -118,37 +118,19 @@ function Login() {
                     </form>
 
                     <div className="login-divider">
-                        <span>or continue with</span>
+                        <span>or</span>
                     </div>
 
-                    <div className="login-social">
-                        <button
-                            className="login-social-btn"
-                            onClick={() => handleSocialLogin('google')}
-                            type="button"
-                        >
-                            <svg viewBox="0 0 24 24" width="18" height="18">
-                                <path fill="#4285F4" d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"/>
-                                <path fill="#34A853" d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"/>
-                                <path fill="#FBBC05" d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"/>
-                                <path fill="#EA4335" d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"/>
-                            </svg>
-                            Google
-                        </button>
-                        <button
-                            className="login-social-btn"
-                            onClick={() => handleSocialLogin('microsoft')}
-                            type="button"
-                        >
-                            <svg viewBox="0 0 21 21" width="18" height="18">
-                                <path fill="#f25022" d="M1 1h9v9H1z"/>
-                                <path fill="#00a4ef" d="M1 11h9v9H1z"/>
-                                <path fill="#7fba00" d="M11 1h9v9h-9z"/>
-                                <path fill="#ffb900" d="M11 11h9v9h-9z"/>
-                            </svg>
-                            Microsoft
-                        </button>
-                    </div>
+                    <button
+                        className="login-sso-btn"
+                        onClick={handleAAFLogin}
+                        type="button"
+                    >
+                        <svg viewBox="0 0 24 24" width="18" height="18" fill="currentColor">
+                            <path d="M12 3L1 9l4 2.18v6L12 21l7-3.82v-6l2-1.09V17h2V9L12 3zm6.82 6L12 12.72 5.18 9 12 5.28 18.82 9zM17 15.99l-5 2.73-5-2.73v-3.72L12 15l5-2.73v3.72z"/>
+                        </svg>
+                        EDU Login
+                    </button>
 
                     <div className="login-footer">
                         Don't have an account?{' '}
